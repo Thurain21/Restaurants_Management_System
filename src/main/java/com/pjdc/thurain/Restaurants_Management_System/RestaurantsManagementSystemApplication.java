d90c36e080a29f03b244887e830b08e731a0db29
Thurain21 <thurainkosg@gmail.com>
2024-10-07 21:31:46 +0800

    add all project files

---
+++ src/main/java/com/pjdc/thurain/Restaurants_Management_System/RestaurantsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pjdc.thurain.Restaurants_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantsManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pjdc/thurain/Restaurants_Management_System/RestaurantsManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjdc.thurain.Restaurants_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantsManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
