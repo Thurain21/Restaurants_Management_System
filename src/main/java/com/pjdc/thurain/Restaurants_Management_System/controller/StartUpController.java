aae115fe90d136a0180877e23b6c3f055df3c331
Thurain21 <thurainkosg@gmail.com>
2024-10-18 19:21:20 +0800

    modified controller for new template

--- src/main/java/com/pjdc/thurain/Restaurants_Management_System/controller/StartUpController.java
+++ src/main/java/com/pjdc/thurain/Restaurants_Management_System/controller/StartUpController.java
@@ -1,13 +1,28 @@
 package com.pjdc.thurain.Restaurants_Management_System.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.pjdc.thurain.Restaurants_Management_System.model.Table;
+import com.pjdc.thurain.Restaurants_Management_System.services.RestaurantServices.ServiceImpl;
+
+@Controller
 public class StartUpController {
 	
+	@Autowired
+	private ServiceImpl sip ;
+	
+	@RequestMapping(value="/RestaurantOrder" , method = RequestMethod.GET)
 	public ModelAndView startUp() {
+		List<Table> tablelist = sip.selectTable();
 		ModelAndView model = new ModelAndView();
-		model.addObject(model);
-		model.setViewName("restaurant");
+		model.addObject("tablelist",tablelist);
+		model.setViewName("RestaruantOrder");
 		return model;
 	}
 
